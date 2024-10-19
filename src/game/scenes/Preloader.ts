8b1f79e938cc9c50e1a0cae4583513ec1ab79f43
Travis Miller <modmatrix@protonmail.com>
2024-10-19 14:36:15 -0400

    Preloader: Add progress bar

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -8,22 +8,59 @@ export default class Preloader extends Scene {
 
   preload () {
 
+    this.createBars()
+    this.setLoadEvents()
+    this.loadImages()
+
+  }
+
+  createBars() {
+
+    this.loadBar = this.add.graphics()
+    this.loadBar.fillStyle(0xd40000, 1)
+    this.loadBar.fillRect(
+      this.cameras.main.width / 4 - 2,
+      this.cameras.main.height / 2 - 18,
+      this.cameras.main.width / 2 + 4, 20
+    )
+    this.progressBar = this.add.graphics()
+  }  
+
+  setLoadEvents() {
+
+    this.load.on('progress', function (value) {
+        this.progressBar.clear()
+        this.progressBar.fillStyle(0x0088aa, 1)
+        this.progressBar.fillRect(
+          this.cameras.main.width / 4,
+          this.cameras.main.height / 2 - 16,
+          (this.cameras.main.width / 2) * value,
+          16
+        )
+      },
+      this
+    )
+
+    this.load.on('complete', () => {
+      this.scene.start('MainMenu');
+    }, this)
+
+  }
+
+  loadImages() {
+
     this.load.spritesheet('hermit', 'assets/dude.png', {
       frameWidth: 32, frameHeight: 48
+    })
+
+    this.load.spritesheet('grass', 'assets/images/grass-spritesheet.png', {
+      frameWidth: 32,
+      frameHeight: 32,
     })
 
     this.load.image('main-menu-bg', 'assets/main-menu-bg.png')
     this.load.image('star', 'assets/star.png')
 
-  }
-
-  create () {
-    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-    //  For example, you can define global animations here, so we can use them in other scenes.
-
-    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start('MainMenu')
-    
   }
 
 }
